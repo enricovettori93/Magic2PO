fb2cc3d88928a3f18147b4e530631d9c2d959f61
Enrico <Enrico@Enrico-PC>
2017-04-13 16:42:12 +0200

    Aggiunto classe boneggiosa volcanic hammer.

---
+++ src/cardgame/cards/VolcanicHammer.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardgame.cards;
+
+import cardgame.AbstractCardEffect;
+import cardgame.Card;
+import cardgame.Effect;
+import cardgame.Player;
+
+/**
+ *
+ * @author giaco
+ */
+public class VolcanicHammer implements Card{
+    
+    private class VolcanicHammerEffect extends AbstractCardEffect {
+        public VolcanicHammerEffect(Player p, Card c) { super(p,c); }
+        @Override
+        public void resolve() {}
+    }
+    @Override
+    public Effect getEffect(Player owner) {
+        return new VolcanicHammerEffect(owner, this);
+    }
+
+    @Override
+    public String name() {
+       return "Volcanic Hammer";
+    }
+
+    @Override
+    public String type() {
+        return "Sorcery";
+    }
+
+    @Override
+    public String ruleText() {
+        return name() + " Volcanic Hammer deals 3 damage to any one creature or player. Fire finds its form in th heat of the forge";
+    }
+    @Override
+    public String toString(){
+        return name() + " (" + type() + ") [" + ruleText() +"]";
+    }
+    @Override
+    public boolean isInstant() {
+        return false;
+    }
+    
+}
