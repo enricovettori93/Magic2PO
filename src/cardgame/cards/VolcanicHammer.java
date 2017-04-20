4a7b03efc2787d86f9da4726fe7082b79d4d6cb1
Enrico <Enrico@Enrico-PC>
2017-04-20 16:58:11 +0200

    Aggiunto il metodo resolve

--- src/cardgame/cards/VolcanicHammer.java
+++ src/cardgame/cards/VolcanicHammer.java
@@ -7,8 +7,10 @@ package cardgame.cards;
 
 import cardgame.AbstractCardEffect;
 import cardgame.Card;
+import cardgame.CardGame;
 import cardgame.Effect;
 import cardgame.Player;
+import java.util.Scanner;
 
 /**
  *
@@ -20,7 +22,44 @@ public class VolcanicHammer implements Card{
         public VolcanicHammerEffect(Player p, Card c) { super(p,c); }
         @Override
         //MANCA METODO RESOLVE
-        public void resolve() {}
+        public void resolve() {
+            int choice,i;
+            choice = 0;
+            Scanner input = new Scanner(System.in);
+            if(CardGame.instance.getCurrentPlayer().getCreatures().isEmpty() && CardGame.instance.getCurrentAdversary().getCreatures().isEmpty()){
+                System.out.println("Would you like ti damage youselves or the opponent? 0 = yourselves, 1 = adversaty");
+                do{
+                    choice = input.nextInt();
+                }while(choice != 0 && choice != 1);
+                if(choice == 0)
+                    CardGame.instance.getCurrentPlayer().inflictDamage(3);
+                else
+                    CardGame.instance.getCurrentAdversary().inflictDamage(3);
+            }
+            else{
+                System.out.println("Would you like to damage one player or all the creature? 0 = player, 1 = creature");
+                do{
+                    choice = input.nextInt();
+                }while(choice != 0 && choice != 1);
+                if(choice == 0){
+                    System.out.println("Would you like ti damage youselves or the opponent? 0 = yourselves, 1 = adversaty");
+                    do{
+                        choice = input.nextInt();
+                    }while(choice != 0 && choice != 1);
+                    if(choice == 0)
+                        CardGame.instance.getCurrentPlayer().inflictDamage(3);
+                    else
+                        CardGame.instance.getCurrentAdversary().inflictDamage(3);
+                }
+                else{
+                    System.out.println("Inflicting 3 damage to all creature in the field.");
+                    for(i = 0;i < CardGame.instance.getCurrentPlayer().getCreatures().size();i++)
+                        CardGame.instance.getCurrentPlayer().getCreatures().get(i).inflictDamage(3);
+                    for(i = 0;i < CardGame.instance.getCurrentAdversary().getCreatures().size();i++)
+                        CardGame.instance.getCurrentAdversary().getCreatures().get(i).inflictDamage(3);
+                }
+            }
+        }
     }
     @Override
     public Effect getEffect(Player owner) {
