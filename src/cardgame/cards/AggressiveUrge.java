2634783085eb3eee1bffcf1222bb976f239432ae
Enrico <Enrico@Enrico-PC>
2017-04-20 09:22:15 +0200

    Aggiunto metodo resolve -> MANCA IL DECORATORE DEL MOSTRO E IL TRIGGER CHE A FINE TURNO SI TOLGA LA CARTA

--- src/cardgame/cards/AggressiveUrge.java
+++ src/cardgame/cards/AggressiveUrge.java
@@ -7,8 +7,10 @@ package cardgame.cards;
 
 import cardgame.AbstractCardEffect;
 import cardgame.Card;
+import cardgame.CardGame;
 import cardgame.Effect;
 import cardgame.Player;
+import java.util.Scanner;
 
 /**
  *
@@ -19,7 +21,36 @@ public class AggressiveUrge implements Card {
         public AggressiveUrgeEffect(Player p, Card c) { super(p,c); }
         @Override
         //MANCA IMPLEMENTAZIONE METODO RESOLVE
-        public void resolve() {}
+        public void resolve() {
+            int powerup = 0;
+            int in;
+            Scanner input = new Scanner(System.in);
+            System.out.println("Player's creature");
+            System.out.println("" + CardGame.instance.getCurrentPlayer().getCreatures());
+            System.out.println("Adversary's creature");
+            System.out.println("" + CardGame.instance.getCurrentAdversary().getCreatures());
+            System.out.println("Select player creature target -> 0 = player, 1 = adversary");
+            do{
+                in =  input.nextInt();
+            }while(in != 0 || in != 1);
+            System.out.println("Select creature");
+            if(in == 0){
+                System.out.println("" + CardGame.instance.getCurrentPlayer().getCreatures());
+                do{
+                    in = input.nextInt();
+                }while(in < 0 || in > CardGame.instance.getCurrentPlayer().getCreatures().size());
+                //DA DEFINIRE DECORATORE PER AUMENTARE ATTACCO E DIFESA DELLA CREATURA
+                CardGame.instance.getCurrentPlayer().getCreatures().get(in+1).DECORATOREAUMENTAATKDEF
+            }
+            else{
+                System.out.println("" + CardGame.instance.getCurrentAdversary().getCreatures());
+                do{
+                    in = input.nextInt();
+                }while(in < 0 || in > CardGame.instance.getCurrentAdversary().getCreatures().size());
+                //DA DEFINIRE DECORATORE PER AUMENTARE ATTACCO E DIFESA DELLA CREATURA
+                CardGame.instance.getCurrentAdversary().getCreatures().get(in+1).DECORATOREAUMENTAATKDEF
+            }
+        }
     }
 
     @Override
