7de78a963faedf6e9c220dc6827f20d826aff3fc
giaco <giaco@DESKTOP-UU6I2V7>
2017-04-27 09:51:08 +0200

    PreventAllDamage aggiunta (da settare con Darkness)

---
+++ src/cardgame/playerstrategy/PreventAllDamageStrategy.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardgame.playerstrategy;
+
+import cardgame.Player;
+
+/**
+ *
+ * @author giaco
+ */
+public class PreventAllDamageStrategy implements PlayerInflictDamageStrategy{
+
+    @Override
+    public void inflictDamage(Player player, int pts) {
+       /*do nothing*/
+    }
+    
+}
