5a3afb4e766165c9b8e9445cfc829ea712be16ae
Enrico <Enrico@Enrico-PC>
2017-05-04 15:04:57 +0200

    Carta dovrebbe essere ok

--- src/cardgame/cards/BenevolentAncestor.java
+++ src/cardgame/cards/BenevolentAncestor.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cardgame.cards;
 
 import cardgame.AbstractCreature;
@@ -12,8 +7,15 @@ import cardgame.CardGame;
 import cardgame.Creature;
 import cardgame.Effect;
 import cardgame.Player;
+import cardgame.TriggerAction;
+import cardgame.Triggers;
+import cardgame.decorator.PowerUpDecorator;
+import cardgame.playerstrategy.*;
+import cardgame.target.Target;
+import cardgame.target.TargetManager;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -33,6 +35,9 @@ public class BenevolentAncestor implements Card{
     private class BenevolentAncestorCreature extends AbstractCreature {
         ArrayList<Effect> all_effects= new ArrayList<>();
         ArrayList<Effect> tap_effects= new ArrayList<>();
+        Target t;
+        int type = 0; //2 = creature, 1 = player
+        int playerchoice = 0; //2 = Adversary, 1 = Current player
         
         BenevolentAncestorCreature(Player owner) { 
             super(owner);
@@ -43,12 +48,71 @@ public class BenevolentAncestor implements Card{
                                         return tap(); 
                                     }
                                     @Override
-                                    public void resolve() {}
+                                    public void resolve() {
+                                        Scanner in = new Scanner(System.in);
+                                        int choice=0;
+                                        System.out.println("Would you like to prevent 1 damage to player or creature?");
+                                        do{
+                                            System.out.println("1 - Player\n2 - Creature");
+                                            choice = in.nextInt();
+                                            type = choice;
+                                        }while(choice == 0);
+                                        if(choice == 2){
+                                            t = (Target)CardGame.instance.getTargetManager().getTarget(TargetManager.CREATURE_ON_FIELD_TARGET);
+                                            ((Creature)t.getTarget()).addDecorator(new PowerUpDecorator("BenevolentAncestorDecorator",0,1));
+                                        }
+                                        else{
+                                            System.out.println("Select player?");
+                                            do{
+                                                System.out.println("1 - Current player\n2 - Adversary");
+                                                choice = in.nextInt();
+                                                playerchoice = choice;
+                                            }while(choice == 0);
+                                            if(choice == 1)
+                                                CardGame.instance.getCurrentPlayer().setInflictDmgStrategy(new PreventOneDamageStrategy());
+                                            else
+                                                CardGame.instance.getCurrentAdversary().setInflictDmgStrategy(new PreventOneDamageStrategy());
+                                        }
+                                    }
                                     @Override
                                     public String toString() 
                                         { return "tap: Prevent the next 1 damage that would be dealt to target creature or player this turn."; }
                                 }
                 ); 
+        }
+        
+        private final TriggerAction BenevolentAncestorTrigger = new TriggerAction(){
+            @Override
+            public void execute(Object args) {
+                if(type != 0){
+                        System.out.println("[BENEVOLENT ANCESTOR] End of turn, removing tap effect from player.");
+                    if(type == 1){
+                        System.out.println("Reset player damage to default");
+                        if(playerchoice == 1)
+                            CardGame.instance.getCurrentPlayer().setInflictDmgStrategy(new DefaultInflictDamage());
+                        else
+                            CardGame.instance.getCurrentAdversary().setInflictDmgStrategy(new DefaultInflictDamage());
+                    }
+                    else{
+                        System.out.println("[BENEVOLENT ANCESTOR] End of turn, removing tap effect from creature.");
+                        ((Creature)t.getTarget()).removeDecorator("BenevolentAncestorDecorator");
+                    }                
+                }
+                type = 0;
+            }   
+        };
+
+        @Override
+        public void insert() {
+            super.insert();
+            //setTarget();
+            CardGame.instance.getTriggers().register(Triggers.END_FILTER,BenevolentAncestorTrigger);
+        }
+
+        @Override
+        public void remove() {
+            super.remove();
+            CardGame.instance.getTriggers().deregister(BenevolentAncestorTrigger);
         }
         
         @Override
