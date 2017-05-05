32f12a501fa196ecd207df1a2decb97662112106
giaco <giaco@DESKTOP-UU6I2V7>
2017-05-05 21:24:11 +0200

    commentata Afflict

--- src/cardgame/cards/Afflict.java
+++ src/cardgame/cards/Afflict.java
@@ -14,22 +14,24 @@ public class Afflict implements Card{
         
         public AfflictEffect (Player p, Card c){
             super(p,c);
-            //
         }
         
         @Override
-    public boolean play() {
-            try{
-                setTarget();
-                return super.play();
-            }
-            catch(Exception e){
-                System.out.println("[AFFLICT] No target avaiable.");
-                return false;
-            }
+        public boolean play() {
+                try{
+                    setTarget(); //setto target
+                    return super.play();
+                }
+                catch(Exception e){
+                    System.out.println("[AFFLICT] No target avaiable.");
+                    return false;
+                }
         }
-        
-    @Override
+        /**
+         * RESOLVE -> SE la vita del target è pari a 1, rimuovo la creatura (andrebbe a -1 quindi muore)
+         *          Se invece è maggiore di -1, applico un Decoratore -1 / -1
+         */
+        @Override
         public void resolve(){
             CreatureTarget=(AbstractCreature)targets.remove(0).getTarget();
             // Aggiungere decoratore al Target - nell'else perchè è quando si può eseguire tale azione
@@ -38,28 +40,31 @@ public class Afflict implements Card{
                 // rimuovo la carta perchè la vita va a 0
             }
             else {
-                PuD = new PowerUpDecorator(AfflictTrigger, -1, -1);
+                //applico un decoratore al target
+                PuD = new PowerUpDecorator(AfflictTrigger, -1, -1); 
                 CreatureTarget.addDecorator(PuD);
-                insert();
+                insert(); 
             }
         }
         
         public void setTarget()throws Exception{
             try{
+                //i target sono le creature nel campo
                 targets.add(CardGame.instance.getTargetManager().getTarget(TargetManager.CREATURE_ON_FIELD_TARGET));
             }catch(Exception e){
-                throw new Exception(e.getMessage());
+                throw new Exception(e.getMessage()); //vuol dire che non ci sono target
             }
         }
         
         private TriggerAction AfflictTrigger=new TriggerAction() {
             @Override
             public void execute(Object args) {
-                CreatureTarget.removeDecorator(this);
+                CreatureTarget.removeDecorator(this); //rimuove il decoratore
             }
         };
 
         public void insert() {
+            //inserisco il trigger, viene  eseguito alla fine del turno
             CardGame.instance.getTriggers().register(Triggers.END_FILTER, AfflictTrigger);
         }
 
