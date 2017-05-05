84459da0ac151ae306941c920c356bde0a1a449a
giaco <giaco@DESKTOP-UU6I2V7>
2017-05-05 20:35:25 +0200

    fix Combat

--- src/cardgame/DefaultExecuteBattleStrategy.java
+++ src/cardgame/DefaultExecuteBattleStrategy.java
@@ -9,14 +9,14 @@ public class DefaultExecuteBattleStrategy implements ExecuteBattleStrategy {
     @Override
     public void executeBattle(DefaultCombatPhase combat,LinkedHashMap<Creature, ArrayList<Creature>> fight) {
         for (Map.Entry<Creature,ArrayList<Creature>> entry : fight.entrySet()) {
-
+            if(!entry.getValue().isEmpty())
                 for(Creature defender : entry.getValue()){
                     if(entry.getKey().getPowerLeft()>0){
                         System.out.println("["+entry.getKey().name()+"] attack the defender "+defender.name());
                         defender.defend(entry.getKey()); /*attacca creatura*/
                     }
                 }
-                if(entry.getKey().getPowerLeft()>0){
+                else{
                     System.out.println("["+entry.getKey().name()+"] attack the rival!");
                     entry.getKey().attack();
                 }
--- src/cardgame/PreventAllDamageStrategy.java
+++ src/cardgame/PreventAllDamageStrategy.java
@@ -9,9 +9,11 @@ public class PreventAllDamageStrategy implements ExecuteBattleStrategy{
     @Override
     public void executeBattle(DefaultCombatPhase combat,LinkedHashMap<Creature, ArrayList<Creature>> fight) {
         for (Map.Entry<Creature,ArrayList<Creature>> entry : fight.entrySet()) {
-
+            if(!entry.getValue().isEmpty()){
                 for(Creature defender : entry.getValue())
                         System.out.println("["+entry.getKey().name()+"] attack the defender "+defender.name()+" [PREVENT ALL DAMAGE ACTIVATED]");
+            }
+            else
                 System.out.println("["+entry.getKey().name()+"] attack the rival! [PREVENT ALL DAMAGE ACTIVATED]");
         }
     }
