c8cef146061d5a6f9d093b375894e2903567bdc2
giaco <giaco@DESKTOP-UU6I2V7>
2017-05-05 21:13:52 +0200

    commentata AetherFlash

--- src/cardgame/cards/AetherFlash.java
+++ src/cardgame/cards/AetherFlash.java
@@ -20,13 +20,16 @@ public class AetherFlash implements Card{
         private AetherFlashEnchantment(Player owner){
             super(owner);
         }
+        /**
+         * TRIGGER AETHERFLASH: ogni volta che una creatura entra in campo, vengono inflitti 2 danni
+         */
         private final TriggerAction AetherFlashAction = new TriggerAction() {
                 @Override
                 public void execute(Object args) {
                     if (args != null  && args instanceof Creature) {
                         Creature c = (Creature)args;
                         System.out.println("[AETHER FLASH] inflict 2 damage to "+c.name());
-                        c.inflictDamage(2);
+                        c.inflictDamage(2); //infliggo 2 danni alla creatura
                     }
                 }
             };
@@ -34,12 +37,14 @@ public class AetherFlash implements Card{
         @Override
         public void insert() {
             super.insert();
+            //inserisco il trigger di AetherFlash
             CardGame.instance.getTriggers().register(Triggers.ENTER_CREATURE_FILTER, AetherFlashAction);
         }
         
         @Override
         public void remove() {
             super.remove();
+            //rimuovo il trigger
             CardGame.instance.getTriggers().deregister(AetherFlashAction);
         }
         
