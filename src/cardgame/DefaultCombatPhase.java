0118b1e47f53c1633aef137377302e77e0bc5e75
giaco <giaco@DESKTOP-UU6I2V7>
2017-05-05 11:40:04 +0200

    aggiornata combat phase

--- src/cardgame/DefaultCombatPhase.java
+++ src/cardgame/DefaultCombatPhase.java
@@ -11,41 +11,61 @@ public class DefaultCombatPhase implements Phase {
     Player opponent;
     ExecuteBattleStrategy ebs = new DefaultExecuteBattleStrategy();
     LinkedHashMap<Creature, ArrayList<Creature>> fight=new LinkedHashMap<>();
+    private void stackInsert(Player actPlayer, Player enemy, boolean isMain){
+        boolean selP1=true, selP2=true;
+        do{
+            selP1=playAvailableEffect(actPlayer, isMain);
+            if(selP2==false && selP2==selP1) //tiene conto della scelta fatta prima
+                selP2=false;
+            else
+                selP2=playAvailableEffect(enemy, !isMain);
+        }
+        while(selP1 || selP2); /*se nessuno sceglie (selP1=false, selP2=false) allora esce*/
+    }
     private boolean playAvailableEffect(Player activePlayer, boolean isMain) {
         //collect and display available effects...
         ArrayList<Effect> availableEffects = new ArrayList<>();
         Scanner reader = CardGame.instance.getScanner();
 
-        //...cards first
+       
         System.out.println(activePlayer.name() + " select card/effect to play, 0 to pass");
-        int i=0;
-        for( Card c:activePlayer.getHand() ) {
-            if ( isMain || c.isInstant() ) {
-                availableEffects.add( c.getEffect(activePlayer) );
-                System.out.println(Integer.toString(i+1)+") " + c );
-                ++i;
-            }
-        }
-        
-        //...creature effects last
-        for ( Creature c:activePlayer.getCreatures()) {
-            for (Effect e:c.avaliableEffects()) {
-                availableEffects.add(e);
-                System.out.println(Integer.toString(i+1)+") " + c.name() + 
-                    " ["+ e + "]" );
-                ++i;
-            }
-        }
-        
-        //get user choice and play it
-        int idx= reader.nextInt()-1;
-        if (idx<0 || idx>=availableEffects.size()) return false;
-
-        availableEffects.get(idx).play();
-        return true;
+        int i, idx;
+        do{
+            do{
+                i=0;
+                idx=0;
+                 //...cards first
+                for( Card c:activePlayer.getHand() ) {
+                    if ( isMain || c.isInstant() ) {
+                        availableEffects.add( c.getEffect(activePlayer) );
+                        System.out.println(Integer.toString(i+1)+") " + c );
+                        ++i;
+                    }
+                }
+                //...creature effects last
+                for ( Creature c:activePlayer.getCreatures()) {
+                    for (Effect e:c.avaliableEffects()) {
+                        availableEffects.add(e);
+                        System.out.println(Integer.toString(i+1)+") " + c.name() + 
+                            " ["+ e + "]" );
+                        ++i;
+                    }
+                }
+                //get user choice and play it
+                idx= reader.nextInt()-1;
+                if (idx<-1 || idx>=availableEffects.size())
+                    System.out.println("[ERR] select correctly!");
+                }while(idx<-1 || idx>=availableEffects.size());
+                if(idx==-1){
+                    System.out.println("You pass");
+                    return false;
+                }
+                
+        }while(!availableEffects.get(idx).play());
+            return  true;
     }
     
-    private void declareAttacker(){
+    private boolean declareAttacker(){
          Scanner reader = CardGame.instance.getScanner();
          int i=0, idx;
          ArrayList<Creature> canAttack = new ArrayList<>();
@@ -54,6 +74,8 @@ public class DefaultCombatPhase implements Phase {
                         canAttack.add(c);
          }
         //Dichiarazione degli attaccanti
+        if(canAttack.isEmpty())
+            return false;
         do{
             System.out.println(currentPlayer.name()+", select the creature that must attack, 0 to pass");
             for(Creature d: canAttack){
@@ -67,17 +89,20 @@ public class DefaultCombatPhase implements Phase {
             }
             i=0;
         }while(idx!=0 && !canAttack.isEmpty());
+        return idx==0 ? false : true;
     }
     
-    private void declareDefender(){
+    private boolean declareDefender(){
         Scanner reader = CardGame.instance.getScanner();
-        int idx;
+        int idx=0;
         ArrayList<Creature> canDefend = new ArrayList<>();
         ArrayList<Creature> attacker = new ArrayList<>();
         for(Creature c:opponent.getCreatures())
             if(!c.isTapped())
                 canDefend.add(c);
         //Dichiarazione dei difensori
+        if(canDefend.isEmpty())
+            return false;
         for(Map.Entry<Creature, ArrayList<Creature>> m: fight.entrySet())
             attacker.add(m.getKey());
         if(!fight.isEmpty()){ /*se c'è almeno un attaccante...*/
@@ -90,33 +115,33 @@ public class DefaultCombatPhase implements Phase {
                     i++;
                 }
                 idx=reader.nextInt();
-                    if(idx!=0 && (idx-1)<canDefend.size()){
-                        Creature defender = canDefend.get(idx-1);
-                        canDefend.remove(idx-1);
-                        int idxDif;
-                        int j=0;
-                        do{
-                            System.out.println(opponent.name()+", select the creature from which you have to defend yourself");
-                            System.out.println("Opponent's attackers:");
-                            for(Creature c : attacker){
-                                System.out.println((j+1)+" "+c.toString());
-                                j++;
-                            }
-                            //TODO: Fase Effetti Stack e istantanei sulla sungola creatura(Difensore)
-                            //playAvailableEffect(opponent,false); boh si fa dopo
-                            //definizione dei difensori rispetto agli attaccanti
-                            idxDif=reader.nextInt();
-                            if(idxDif>attacker.size() && idxDif<1)
-                                System.out.println("Error: there isn't any attacker in that position. Reselect");
-                        }while(idxDif>attacker.size() && idxDif<1);
-                        Creature att = attacker.get(idxDif-1);
-                        ArrayList<Creature> defendFrom = fight.get(att);
-                        defendFrom.add(defender);
-                        fight.put(att, defendFrom);
-                        playAvailableEffect(opponent, false);
-                    }
-                }while(idx!=0);
+                if(idx!=0 && (idx-1)<canDefend.size()){
+                    Creature defender = canDefend.get(idx-1);
+                    canDefend.remove(idx-1);
+                    int idxDif=0;
+                    int j=0;
+                    do{
+                        System.out.println(opponent.name()+", select the creature from which you have to defend yourself");
+                        System.out.println("Opponent's attackers:");
+                        for(Creature c : attacker){
+                            System.out.println((j+1)+" "+c.toString());
+                            j++;
+                        }
+                        //TODO: Fase Effetti Stack e istantanei sulla sungola creatura(Difensore)
+                        //playAvailableEffect(opponent,false); boh si fa dopo
+                        //definizione dei difensori rispetto agli attaccanti
+                        idxDif=reader.nextInt();
+                        if(idxDif>attacker.size() && idxDif<1)
+                            System.out.println("Error: there isn't any attacker in that position. Reselect");
+                    }while(idxDif>attacker.size() && idxDif<1);
+                    Creature att = attacker.get(idxDif-1);
+                    ArrayList<Creature> defendFrom = fight.get(att);
+                    defendFrom.add(defender);
+                    fight.put(att, defendFrom);
+                }
+            }while(idx!=0 && !canDefend.isEmpty());
         }
+        return idx==0 ? false : true;
     }
     
     private void executeBattle(){
@@ -131,15 +156,15 @@ public class DefaultCombatPhase implements Phase {
        
         CardGame.instance.getTriggers().trigger(Triggers.COMBAT_FILTER);
 
-        if(!currentPlayer.getCreatures().isEmpty()){ //nel programma del prof., le creature giocate sono contenute in "Player"
-           declareAttacker();
-           playAvailableEffect(opponent, false);
+        
+           if(declareAttacker()){
+                stackInsert(opponent, currentPlayer, false);
+                if(declareDefender())
+                    stackInsert(currentPlayer, opponent, true );
            CardGame.instance.getStack().resolve();
-           declareDefender();
-           
            executeBattle();
+           }
            System.out.println("[END_COMBAT_PHASE] STATUS: "+currentPlayer.name()+" LIFE:"+currentPlayer.getLife()+" "+opponent.name()+" LIFE:"+opponent.getLife());
-        }
     }
     
     public void setEbs(ExecuteBattleStrategy ebs){
